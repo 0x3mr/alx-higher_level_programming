6533012507beb90fdf11ede490f1b043f8dcf33f
Amr Abdelfattah <amrabdelfattah321@gmail.com>
2023-11-09 02:27:30 +0200

    print_comb2

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
 
+printf = ""
+
 for num in range(0, 100):
     if num in range(0, 10):
-        print(f"0{num}, ", end='')
+        printf += f"0{num}, "
     elif num == 99:
-        print("99")
+        printf += "99"
     else:
-        print(f"{num}, ", end='')
+        printf += f"{num}, "
+print(printf, end='')
