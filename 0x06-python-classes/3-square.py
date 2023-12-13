dd554f715af48a2f810dc32859b570fc4766425a
Amr <amrabdelfattah321@gmail.com>
2023-12-13 21:11:05 +0200

    Classes 4rth file

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+square class
+"""
+
+
+class Square:
+    """
+    square class
+
+    Attributes:
+        __size (int): square size
+    """
+    def __init__(self, size=0):
+        """
+        Initialize the square instance.
+        """
+
+        if not type(size) is int:
+            raise (TypeError("size must be an integer"))
+        elif size < 0:
+            raise (TypeError("size must be >= 0"))
+        self.__size = size
+
+    def area(self):
+        """
+        calc square size instance
+        """
+        return self.__size * self.__size
