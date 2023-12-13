671ec098daebae7a43bf39b97e1770ec4af84015
Amr <amrabdelfattah321@gmail.com>
2023-12-13 17:24:16 +0200

    Classes 3rd file

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+square class
+"""
+
+
+class Square:
+    """
+    square class
+
+    Attributes:
+        __size (int): square size
+    """
+    def __init__(self, size=0):
+        """
+        Initialize the square instance.
+        """
+
+        if not type(size) is int:
+            raise (TypeError("size must be an integer"))
+        elif size < 0:
+            raise (TypeError("size must be >= 0"))
+        self.__size = size
