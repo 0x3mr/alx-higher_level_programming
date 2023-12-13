807ea777256a2d792b3d4f2a92bbb71a399d2ed0
Amr <amrabdelfattah321@gmail.com>
2023-12-13 16:43:41 +0200

    Classes second file

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+square class
+"""
+
+
+class Square:
+    """
+    square class
+        Attributes:
+            __size (int): square size
+    """
+
+    def __init__(self, size=None):
+        self.__size = size
