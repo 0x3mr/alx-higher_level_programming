4df841dbb2204c8838ef662f3ecea6e55d95c2ec
Amr <amrabdelfattah321@gmail.com>
2024-04-03 03:45:09 +0200

    testerino

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,7 @@
 #!/usr/bin/node
-const langs = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+// Uses an array to loop through and print strings
 
+const langs = ['C is fun', 'Python is cool', 'Javascript is amazing'];
 for (let i = 0; i < langs.length; i++) {
-    console.log(langs[i]);
+  console.log(langs[i]);
 }
